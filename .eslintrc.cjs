ac1936426fd32eb02f746eb2c2224e429dba98f0
dimasmds <dimas.maulanads@gmail.com>
2025-10-21 16:19:11 +0700

    fix: add node environment to ESLint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,6 +1,7 @@
 module.exports = {
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
-  root: false,
+    env: { node: true },
+    extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+    parser: '@typescript-eslint/parser',
+    plugins: ['@typescript-eslint'],
+    root: false,
 };
