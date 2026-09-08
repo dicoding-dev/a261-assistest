466e617572bcc98f261f476dae77eed574983683
Fauzi Kurniawan <kirigaya.kurniawan@gmail.com>
2026-09-08 15:19:48 +0700

    feat: add eslintConfig to PackageJson interface and enhance Dependencies type

--- src/entities/submission-project/package-json.ts
+++ src/entities/submission-project/package-json.ts
@@ -2,10 +2,12 @@ interface PackageJson {
     scripts?: object
     dependencies?: Dependencies
     devDependencies?: Dependencies
+    eslintConfig?: object
 }
 
 interface Dependencies{
     eslint?: string
+    [packageName: string]: string | undefined
 }
 
 export default PackageJson
